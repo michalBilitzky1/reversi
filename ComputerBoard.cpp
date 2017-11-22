ff15c948f4e86505596f2da29a21a993ed524b8b
Miri Asher <miriasher@gmail.com>
2017-11-22 14:42:06 +0200

    new class computer board

--- ComputerBoard.cpp
+++ ComputerBoard.cpp
@@ -12,15 +12,14 @@ ComputerBoard::ComputerBoard(Board &board, Steps steps, Player player): board(&b
 }
 
 void ComputerBoard::checkMoveComputer(){
-    vector<int> vectorOfGradeCom;
+
     steps.optionsToLocate(player.getPlayerO());
     vector<Piece> vector=steps.getVec();//וקטור מחזיק את כל האופציות שיש למחשב כעת על כל אופציה בנפרד נעשה פליפ
+
     //maybe the size sould be -1
     for(int i=0; i<vector.size();i++){
         steps.pieceToFlip(vector.at(i),player.getPlayerO());//עשה פליפ לאופצייה הI
         checkMoveHuman();//עושה אותו דבר לשחן האנושי
-        int grade=grade();
-        vectorOfGradeCom.push_back(grade);
     }
 
 }
@@ -29,4 +28,16 @@ int ComputerBoard::grade() {
     int grade;
     grade=player.countX()-player.countO();
     return grade;
+}
+
+vector<int> ComputerBoard::checkMoveHuman(){
+    vector<int> vectorOfGradeCom;
+    steps.optionsToLocate(player.getPlayerX());
+    vector<Piece> vectorToHuman=steps.getVec();
+    for(int j=0; j<vectorToHuman.size();j++){
+        steps.pieceToFlip(vectorToHuman.at(j),player.getPlayerX());
+        int grade=grade();
+        vectorOfGradeCom.push_back(grade);
+    }
+    return vectorOfGradeCom;
 }
--- ComputerBoard.h
+++ ComputerBoard.h
@@ -21,6 +21,7 @@ public:
 
     void checkMoveComputer();
     int grade();
+    vector<int> checkMoveHuman();
 };
 
 
